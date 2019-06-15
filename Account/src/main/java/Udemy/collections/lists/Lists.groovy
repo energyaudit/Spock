f73ec0ec4068c40c8ffadab95d8eee24563201d9
Billy Yang <energyaudit668@gmail.com>
2019-06-15 18:34:19 -0400

    modified collection listas and maps

--- Account/src/main/java/Udemy/collections/lists/Lists.groovy
+++ Account/src/main/java/Udemy/collections/lists/Lists.groovy
@@ -15,24 +15,29 @@ println nums2.class.name
 // add | remove | get | clear
 
 nums.push(99)
+println nums
 nums.putAt(0,77)
+println "put at (0,77)"+ nums
 nums[0] = 78
+println nums
 nums + 7
+println " + 7"+ nums
 nums << 66
+println "<<66"+nums
 
 nums.pop()
 nums.removeAt(0)
 def newList = nums - 3
-println newList
+println "-3 will remove all 3 in list"+newList
 
 println nums.getAt(0..3)
 nums = []
-
+println "assign to a empty list will cear the list"+nums
 // flatten
 nums << [3,4,5]
 nums << [1,2]
 println nums
-println nums.flatten()
+println "fattern"+nums.flatten()
 
 // equals
 def myNumbers = [1,2,3]
@@ -44,6 +49,11 @@ println nums.findAll { it == 4 }
 println nums.flatten().findAll { it < 5 }
 
 // getAt(Range)
+nums << 66
+nums << 67
+println nums
+nums=nums.flatten()
+println nums.size()+"num size"
 println nums.getAt(0..5)
 
 // reverse list
@@ -57,3 +67,6 @@ def evens = [10,2,8,4,24,14,2] as Set
 println evens
 println evens.class.name
 
+def evens1 = [10,2,8,4,24,14,2,6] as SortedSet
+println evens1
+println evens1.class.name
--- Account/src/main/java/Udemy/collections/maps/Maps.groovy
+++ Account/src/main/java/Udemy/collections/maps/Maps.groovy
@@ -11,9 +11,12 @@ println person.first
 person.twitter = "@therealdanvega"
 println person
 
+def twitter1=[username:"@therealdanvega",'email address':"danvega@gmail.com"]
+println "normally key no need quote but if there is space between word ,must add single quote"+twitter1
+
 def emailKey = "EmailAddress"
 def twitter = [username:"@therealdanvega",(emailKey):"danvega@gmail.com"]
-
+println "if variable as key ,it must quoted by ()"+twitter1
 println person.size()
 println person.sort()
 
